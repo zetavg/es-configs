e24643ba53d44b75a3ff5d469f11378bc99a3399
zetavg <mail@zeta.vg>
2026-04-06 05:40:35 +0200

    fix(eslint-config): disable `@typescript-eslint/no-unused-vars` in favor of `no-unused-vars`

--- configs/eslint-config/configs/typescript.js
+++ configs/eslint-config/configs/typescript.js
@@ -4,4 +4,12 @@ import tseslint from 'typescript-eslint';
 /**
  * Default ESLint configuration for TypeScript.
  */
-export default defineConfig([tseslint.configs.recommended]);
+export default defineConfig([
+  tseslint.configs.recommended,
+  {
+    rules: {
+      // Turned off in favor of the `no-unused/no-unused-vars` rule in `no-unused.js`.
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+]);
