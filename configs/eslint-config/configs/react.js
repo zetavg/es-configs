4580da279352c185f98461be6c235c2a204c6874
zetavg <mail@zeta.vg>
2026-04-06 03:51:45 +0200

    fix(eslint-config): hardcode React version to 19 to work around ESLint v10 API removal

--- configs/eslint-config/configs/react.js
+++ configs/eslint-config/configs/react.js
@@ -15,7 +15,11 @@ export default defineConfig([
     },
     settings: {
       react: {
-        version: 'detect',
+        // eslint-plugin-react uses the legacy `context.getFilename()` API for version auto-detection, which was removed in ESLint v10.
+        // To work around this, we can only specify the React version explicitly. We should update this when eslint-plugin-react releases a new version that supports the new API.
+        // See: https://github.com/vercel/next.js/issues/89764#issuecomment-3928272828
+        // version: 'detect',
+        version: '19',
       },
     },
   },
